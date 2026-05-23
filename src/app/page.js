db48fea37286f3c65ea6da81240825c660ab2a37
Sammychvz17 <sammy_chvz17@outlook.com>
2026-05-22 23:19:38 -0500

    added ids and classes for trailwind

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,23 @@
 import react from 'react';
+import './globals.css';
+
 
 const HomePage = () => {
 
     return (
         <div>
+         <div className="container mx-auto px-4 py-8">      
             <h1>Welcome to the Home Page</h1>
+        </div>
             <p>This is the main landing page of our application.</p>
+            <nav>
+                <ul id="nav-list">
+                    <li id="home-link"><a href="/">Home</a></li>
+                    <li id="about-link"><a href="/about">About</a></li>
+                    <li id="contact-link"><a href="/contact">Contact</a></li>
+                    <li id="projects-link"><a href="/projects">Projects</a></li>
+                </ul>
+            </nav>
         </div>
     );
 };
