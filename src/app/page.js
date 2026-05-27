754813e72ff3597447d054c41b71da5c15509887
Sammychvz17 <sammy_chvz17@outlook.com>
2026-05-26 23:12:31 -0500

    added some motion to the home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+"use client";
+
 import react from 'react';
 import { motion, AnimatePresence } from "motion/react";
 import './globals.css';
@@ -7,27 +9,48 @@ const HomePage = () => {
 
     return (
     <div>
-
-        <div className="">      
-            <h1 className="text-left text-4xl mb-4 p-14 w-full">Samantha Chavez</h1>
-            <nav className="text-right mt-2">
+        <div className="">
+            <AnimatePresence>
+                <motion.div
+                    initial={{ opacity: 0, y: -40 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.9 }}
+                    className="text-center pd-4"
+                >
+                    <h1 className="text-left text-4xl mb-4 p-14 w-full">Samantha Chavez</h1>
+                </motion.div>
+            </AnimatePresence>
+        
+            <nav className="text-center mt-2">
                 <ul id="nav-list" className= "text-md" >
-                    <li id="about-link" className= "hover:shadow-lg text-gray-800"><a href="./about">About</a></li>
-                    <li id="contact-link" className= "hover:shadow-lg text-gray-800"><a href="./contact">Contact</a></li>
-                    <li id="projects-link" className= "hover:shadow-lg text-gray-800"><a href="./projects">Projects</a></li>
+                    <motion.li
+                        whileHover={{ scale: 1.1 }}
+                        id="home-link" className= "hover:shadow-lg text-gray-800"><a href="./">Home</a></motion.li>
+                    <motion.li
+                        whileHover={{ scale: 1.1 }}
+                        id="about-link" className= "hover:shadow-lg text-gray-800"><a href="./about">About</a></motion.li>
+                    <motion.li
+                        whileHover={{ scale: 1.1 }}
+                        id="contact-link" className= "hover:shadow-lg text-gray-800"><a href="./contact">Contact</a></motion.li>
+                    <motion.li
+                        whileHover={{ scale: 1.1 }}
+                        id="projects-link" className= "hover:shadow-lg text-gray-800"><a href="./projects">Projects</a></motion.li>
                 </ul>
             </nav>
         </div>
-
-       
-        
+    
         <div className="mt-6">  
             
         </div>
         <br />
-        <div className="text-center pd-4">
+
+        <motion.div 
+            initial={{ opacity: 0, y: 40 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8 }}className="text-center pd-4"
+        >
             <p>lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        </div>
+        </motion.div>
 
     </div>
     );
