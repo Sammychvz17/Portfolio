06a4cfe6dc4b355e4cb07e6f3f4a1a335e648088
Sammychvz17 <sammy_chvz17@outlook.com>
2026-05-27 22:44:41 -0500

    fixed bug in card componet on main page

--- src/app/page.js
+++ src/app/page.js
@@ -60,7 +60,7 @@ const HomePage = () => {
         >
             <p>lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         </motion.div>
-        <Card title="Project Title" description="Project Description" image="../public/flower.jpg" />
+        <Card title="Project Title" description="Project Description" image="/flower.jpg" />
 
     </div>
     );
