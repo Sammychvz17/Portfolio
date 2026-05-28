bc5c9ca0b95193d86630930ea71d9f9fcb001ff4
Sammychvz17 <sammy_chvz17@outlook.com>
2026-05-27 22:53:47 -0500

    added motion correctly to the card componet

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,18 @@
+import { motion, AnimatePresence } from "motion/react";
 
 function Card({ title, description, image }) {
     return (
-        <div className="card"> 
+        <motion.div 
+            initial={{ opacity: 0, y: 40 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8 }}
+            whileHover={{ scale: 1.05 }}
+            className="card"
+        >
             {image && <img src={image} alt={title} className="card-image" />}
             <h3 className="card-title">{title}</h3>
             <p className="card-description">{description}</p>
-        </div>
+        </motion.div>
     );
 }
 
