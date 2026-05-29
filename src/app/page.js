ce7874f5cf3991f046ea85abceeb8f487e40852b
Sammychvz17 <sammy_chvz17@outlook.com>
2026-05-28 23:17:15 -0500

    added assests folder

--- src/app/page.js
+++ src/app/page.js
@@ -12,6 +12,7 @@ import Projects from '@/components/Projects';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import Card from '@/components/Card';
+import flower from '../assets/images/flower.jpg';
 
 
 const HomePage = () => {
@@ -60,7 +61,7 @@ const HomePage = () => {
         >
             <p>lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         </motion.div>
-            <Card title="Project Title" description="Project Description" image="./img/flower.jpg" />
+            <Card title="Project Title" description="Project Description" image= {flower} />
     </div>
     );
 };
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,7 +9,12 @@ function Card({ title, description, image }) {
             whileHover={{ scale: 1.05 }}
             className="card"
         >
-            {image && <img src={image} alt={title} className="card-image" />}
+        <Image
+        src={image}
+        alt={title}
+        width={400}
+        height={250}
+      />
             <h3 className="card-title">{title}</h3>
             <p className="card-description">{description}</p>
         </motion.div>
