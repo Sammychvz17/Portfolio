f870550941eb967759dd2ebe86f360aaf35c1395
Sammychvz17 <sammy_chvz17@outlook.com>
2026-06-01 21:57:04 -0500

    took out unused lbry from head

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,7 @@
 "use client"; // This directive indicates that the code in this file should be executed on the client side, allowing for interactivity and dynamic content rendering in a React application.
 
 // Importing necessary libraries and components for the HomePage component.
-import react from 'react';
 import { motion, AnimatePresence } from "motion/react";
-import Link from "next/link";
 
 // Importing the global CSS styles and other components that will be used in the HomePage component.
 import './globals.css';
@@ -23,7 +21,7 @@ const HomePage = () => {
     <Navbar />
 
 
-    
+
     <main className="container">
       <motion.section 
         id="home"
