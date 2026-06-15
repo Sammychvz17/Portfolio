a1b8eefa9c822e94594b56a5bd7fcb4d8d9f043d
Sammychvz17 <sammy_chvz17@outlook.com>
2026-06-14 21:43:58 -0500

    added logo to nav bar and to assests folder

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,23 @@
-import { motion, AnimatePresence } from "motion/react";
-import Link from "next/link";
+
+import Image from "next/image";
+import logo from "@/assets/icons/logo.png";
+
 
 
 const Navbar = () => {
     return (
         <nav className="sticky top-0 z-50 backdrop-blur-sm ">
+
             <ul id="nav-list" className="flex justify-end gap-8 py-4 text-lg">
+            <div className="flex justify left">
+            <Image
+                src={logo}
+                alt="Logo"
+                width={100}
+                height={100}
+            />
+
+        </div>
             <li className="hover:underline">
                 <a href="#home">Home</a>
             </li>
