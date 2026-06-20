d450f5440aa9bc56d662ad22d470ecdc4e5d03ab
Sammychvz17 <sammy_chvz17@outlook.com>
2026-06-19 23:46:37 -0500

    took out home button for future logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,21 +6,10 @@ import logo from "@/assets/icons/logo.png";
 
 const Navbar = () => {
     return (
-        <nav className="sticky top-0 z-50 backdrop-blur-sm ">
+        <nav className="sticky top-0 z-50">
 
             <ul id="nav-list" className="flex justify-end gap-8 py-4 text-lg">
-            <div className="flex justify left">
-            <Image
-                src={logo}
-                alt="Logo"
-                width={100}
-                height={100}
-            />
-
-        </div>
-            <li className="hover:underline">
-                <a href="#home">Home</a>
-            </li>
+            
 
             <li className="hover:underline">
                 <a href="#about">About</a>
